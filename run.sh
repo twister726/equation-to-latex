de09829c4c433a798ba4520d6829c3e5cd45e4e7
Megh Parikh <meghprkh@gmail.com>
2017-10-26 12:57:23 +0530

    Add tesseract prediction

--- run.sh
+++ run.sh
@@ -14,4 +14,12 @@ mkdir ../output/segmented
 matlab -nodesktop -nosplash -r "main('../output/rotated_$fname', '../output/segmented/'); exit"
 
 cd ..
+
+cd output/segmented
+for file in `ls -1`; do
+  predicted=`tesseract -l eng+ell+lat --psm 10 $file stdout 2>/dev/null | head -n 1`
+  mv "$file" "${file}_predicted_${predicted}.png"
+done
+cd -
+
 mv "$fname" "output/original.png"
